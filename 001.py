68559b3f0b2559d315b35366aa35e90a818d03dd
Jeffrey Lou <jeffzlou@gmail.com>
2018-09-01 14:32:02 -0400

    standards and such

--- 001.py
+++ 001.py
@@ -7,10 +7,11 @@ we get 3, 5, 6 and 9. The sum of these multiples is 23.
 
 Find the sum of all the multiples of 3 or 5 below 1000.
 """
-def compute():
-  ans = sum(x for x in range(1000) if (x % 3 == 0 or x % 5 == 0))
-  return str(ans)
+
+
+def compute(n):
+    return sum(x for x in range(n) if (x % 3 == 0 or x % 5 == 0))
 
 
 if __name__ == "__main__":
-  print(compute())
+    print(compute(1000))
